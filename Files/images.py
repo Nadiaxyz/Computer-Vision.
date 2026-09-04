03c32f5d2f8e10def225ed886f1453c884f66456
Nadia <153601810+Nadiaxyz@users.noreply.github.com>
2026-09-04 21:40:38 +0500

    Update images.py

--- Files/images.py
+++ Files/images.py
@@ -1,8 +1,8 @@
 import cv2
 
-image = cv2.imread("../Images/image1.jpg")
+image = cv2.imread("../Images/piano (1).jpg")
 
-cv2.imshow("My Image", image)
+cv2.imshow("Shirt Image", image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
